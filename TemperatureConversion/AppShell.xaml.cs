9124cbcd904b27c5a02a085edf6b36cd75f0c1d5
Chad McDaniel <ccm290@msstate.edu>
2023-02-26 16:41:12 -0600

    Add project files.

---
+++ TemperatureConversion/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TemperatureConversion;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TemperatureConversion/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace TemperatureConversion;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void CalcCelsius(object sender, EventArgs e)
+	{
+		try
+		{
+			//Get the entered Fahrenheit from the text box.
+			//NOTE: The try block will catch if the user entered a non-numerical
+			//      number.
+			double fahrenheit = Convert.ToDouble(txtFahrenheit.Text);
+
+			//Convert to Celsius
+			double celsius = (fahrenheit - 32.0) * (5.0 / 9.0);
+
+			//Display the Celsius in the result label.
+			lblCelsiusResult.Text = celsius.ToString("#.#####");
+
+			//Change the color of the label's text to white.
+			//We do this in case the text was previously red from
+			//the catch statement below.
+			lblCelsiusResult.TextColor = Color.Parse("White");
+		}
+		catch(FormatException ex)
+		{
+			//If the user entered a non-numerical temperature, then
+			//output an error message to the result label and set its
+			//text color to red.
+			lblCelsiusResult.Text = "Error: This is not a decimal number.  Enter again...";
+			lblCelsiusResult.TextColor = Color.Parse("Red");
+		}
+	}
+}
+
